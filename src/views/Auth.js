f59962aec48e94ac20eb18fec7574fc28b1cbcd7
spectrox0 <alejanvelazco2008@hotmail.com>
2019-12-04 21:38:51 -0400

    asdas

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,31 +1,25 @@
-import React, {useState, useEffect} from 'react'; 
-import {
-    MDBContainer, 
-    MDBRow, 
-    MDBCol
-} from 'mdbreact';
-import Control from '../components/Auth/controls';
-import SignIn from '../components/Auth/SignIn';
-import SignUp from '../components/Auth/SignUp';
+import React, { useState, useEffect } from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import Control from "../components/Auth/controls";
+import SignIn from "../components/Auth/SignIn";
+import SignUp from "../components/Auth/SignUp";
 
 export default function Auth() {
- 
   const [signIn, setSignIn] = useState(true);
- 
-  const switchSign = (value) => {
-   setSignIn(value);
-  }
 
- return (
-  <div className="pageAuth"> 
-  <MDBContainer className="containerAuth"> 
-   <Control isSignIn={signIn} switchSign={switchSign} />
-     <div className="forms"> 
-     <SignIn isSignIn={signIn}/> 
-     <SignUp isSignIn={signIn}/> 
-     </div>
-  </MDBContainer>
-  </div>
- )
+  const switchSign = value => {
+    setSignIn(value);
+  };
 
+  return (
+    <div className="pageAuth">
+      <MDBContainer className="containerAuth">
+        <Control isSignIn={signIn} switchSign={switchSign} />
+        <div className="forms">
+          <SignIn isSignIn={signIn} />
+          <SignUp isSignIn={signIn} />
+        </div>
+      </MDBContainer>
+    </div>
+  );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,26 +1,19 @@
-import React, {useState, useEffect} from 'react'; 
-import {
-    MDBContainer, 
-    MDBRow, 
-    MDBCol
-} from 'mdbreact';
-import SideBar from '../components/Home/SideBar.js'
-import Chat from '../components/Home/Chat.js'
-
+import React, { useState, useEffect } from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import SideBar from "../components/Home/SideBar.js";
+import Chat from "../components/Home/Chat.js";
 
 export default function Auth() {
-    const [isOpenSideBar, setIsOpenSideBar] =useState(false);
+  const [isOpenSideBar, setIsOpenSideBar] = useState(false);
 
-    const onClick = () => {
-        setIsOpenSideBar(!isOpenSideBar);
-    }
-    
+  const onClick = () => {
+    setIsOpenSideBar(!isOpenSideBar);
+  };
 
- return (
-  <div className="Home"> 
-  <SideBar onClick={onClick} isOpenSideBar={isOpenSideBar} />
-  <Chat onClick={onClick} />
-  </div>
- )
-
+  return (
+    <div className="Home">
+      <SideBar onClick={onClick} isOpenSideBar={isOpenSideBar} />
+      <Chat onClick={onClick} />
+    </div>
+  );
 }
