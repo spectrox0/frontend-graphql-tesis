56e4d6c804f01e307f4dced403ce8f31b42fda67
spectrox0 <alejanvelazco2008@hotmail.com>
2020-01-20 00:38:50 -0400

    sd

---
+++ src/helpers/graphql/subscription/subcription.js
@@ -0,0 +1,42 @@
+import gql from "graphql-tag";
+export const MESSAGE_ADDED_SUBSCRIPTION = gql`
+  subscription messageAdded($postId: String!) {
+    messageAdded(postId: $postId) {
+      _id
+      content
+      date
+      user {
+        _id
+        username
+        name
+        urlImg
+      }
+    }
+  }
+`;
+
+export const NOTIFICATION_ADDED_SUSCRIPTION = gql`
+  subscription notificationAdded($userId: String!) {
+    notificationAdded(userId: $userId) {
+      _id
+      post {
+        title
+        creator {
+          _id
+          username
+        }
+        urlImg
+        _id
+      }
+      message {
+        content
+        date
+
+        user {
+          username
+          urlImg
+        }
+      }
+    }
+  }
+`;
